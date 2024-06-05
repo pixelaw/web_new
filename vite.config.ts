556217ee64d2c80d91854c0ffebb549b1c685504
Caspar Oostendorp <caspar@oostvoort.com>
2024-06-05 13:04:32 +0800

    Add vitest configuration file for test coverage and refactor Vite config formatting

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,12 @@
-/// <reference types="vitest" />
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 import graphqlLoader from "vite-plugin-graphql-loader";
 
-
 // https://vitejs.dev/config/
 export default defineConfig({
+    plugins: [
+        react(),
+        graphqlLoader(),
 
-  plugins: [
-      react(),
-      graphqlLoader()
-  ],
-
+    ],
 })
