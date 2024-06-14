e88fe0c9f1a28cbe290b82720a8e5530f2dd45cc
0xshora <112358132134.fibon@gmail.com>
2024-06-14 23:10:06 +0100

    fix: error about css

--- src/App.tsx
+++ src/App.tsx
@@ -51,18 +51,6 @@ function App() {
         console.log("onCellClick", coordinate)
     }
 
-    const colors = [
-        "#FF0000",
-        "#FF7F00",
-        "#FFFF00",
-        "#00FF00",
-        "#0000FF",
-        "#4B0082",
-        "#9400D3",
-        "#FFFFFF", // white
-        "#000000"  // black
-    ];
-
     return (
         <div className='App bg-bg-primary'>
             <div className='w-full h-12 bg-header-primary flex items-center justify-between px-4'>
