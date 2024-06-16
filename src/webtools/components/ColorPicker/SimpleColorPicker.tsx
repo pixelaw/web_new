6ffe623147e53e1489c0f0c03326348166cf65e8
0xshora <112358132134.fibon@gmail.com>
2024-06-16 23:04:50 +0100

    feat: show New and Old strings to the color palette

--- src/webtools/components/ColorPicker/SimpleColorPicker.tsx
+++ src/webtools/components/ColorPicker/SimpleColorPicker.tsx
@@ -1,34 +1,44 @@
+import React, { useState } from "react";
 import { ColorPickerProps } from "../../types.ts";
 import styles from './SimpleColorPicker.module.css';
 
 const colors = [
-    "#FF0000",
-    "#FF7F00",
-    "#FFFF00",
-    "#00FF00",
-    "#0000FF",
-    "#4B0082",
-    "#9400D3",
-    "#FFFFFF", // white
-    "#000000"  // black
+  "#FF0000",
+  "#FF7F00",
+  "#FFFF00",
+  "#00FF00",
+  "#0000FF",
+  "#4B0082",
+  "#9400D3",
+  "#FFFFFF", // white
+  "#000000"  // black
 ];
 
 const SimpleColorPicker: React.FC<ColorPickerProps> = ({ onColorSelect }) => {
-    return (
-        <div className={styles.inner}>
-            {colors.map((color, index) => (
-                <button
-                    key={index}
-                    style={{ backgroundColor: color }}
-                    className={`${styles.button} ${color === '#FFFFFF' ? styles['button-white'] : ''}`}
-                    aria-label={`Color ${color}`}
-                    onClick={() => onColorSelect(color)}
-                >
-                    <span className={styles.number}>{index + 1}</span>
-                </button>
-            ))}
-        </div>
-    );
+  const [selectedColor, setSelectedColor] = useState<string | null>(null);
+
+  const handleColorSelect = (color: string) => {
+    setSelectedColor(color);
+    onColorSelect(color);
+  };
+
+  return (
+    <div className={styles.inner}>
+      {colors.map((color, index) => (
+        <button
+          key={index}
+          style={{ backgroundColor: color }}
+          className={`${styles.button} ${color === '#FFFFFF' ? styles['button-white'] : ''} ${color === selectedColor ? styles.selected : ''}`}
+          aria-label={`Color ${color}`}
+          onClick={() => handleColorSelect(color)}
+        >
+          <span className={styles.label}>
+            {index === 0 ? 'New' : index === 8 ? 'Old' : ''}
+          </span>
+        </button>
+      ))}
+    </div>
+  );
 };
 
 export default SimpleColorPicker;
